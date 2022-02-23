c8244896050ecaf3425ee2ae97248e91a82859ba
Alemol <91252055+AlemolAMG02@users.noreply.github.com>
2022-02-23 10:39:29 +0100

    UP web.php . Cambio de las rutas.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FestivalController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inicio');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/menuInicio', function () {
+    return view('menuInicio');
+})->middleware(['auth'])->name('menuInicio');
+
+
+Route::resource('festival', FestivalController::class)->middleware(['auth']);;
 
 require __DIR__.'/auth.php';
