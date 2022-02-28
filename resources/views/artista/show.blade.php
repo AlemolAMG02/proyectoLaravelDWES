7a2a9a93e2749f4206d496cc77a00d5c2e9fa25e
Alemol <91252055+AlemolAMG02@users.noreply.github.com>
2022-02-28 13:02:28 +0100

    ADD cambios del lunes

--- resources/views/artista/show.blade.php
+++ resources/views/artista/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Coches') }}
+            {{ __($artist->nombre) }}
         </h2>
     </x-slot>
 
@@ -10,15 +10,21 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-
                     <div class="grid grid-rows-1 grid-flow-col gap-4">
-                        <div><img class="rounded" width="60%" src="{{$artist->foto}}"></div>
+                        <div>
+                            <img class="w-full" src="{{$artist->foto}}" alt="{{$artist->foto}}">
+                        </div>
                         <div>
                             <p class="text-xl">Matricula: {{$artist->nombre}}</p>
                             <p class="text-xl">Descpri: {{$artist->descripcion}}</p>
                             <p class="text-xl">Estilo: {{$artist->estilo}}</p>
-                            <p class="text-xl">Participa en: {{$fest->nombre}}</p>
-
+                            <p class="text-xl">Datos img: {{$artist->foto}}</p>
+                        </div>
+                        <div>
+                            <button type="button"
+                                    class="inline-block bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white px-3 py-1 text-sm font-semibold text-blue-400 mr-2 mb-2">
+                                Reservar Entradas
+                            </button>
                         </div>
                     </div>
 
