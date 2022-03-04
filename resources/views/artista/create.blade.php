99961e280ac5568d444f7749eae2f6202457a873
Alemol <91252055+AlemolAMG02@users.noreply.github.com>
2022-03-04 09:00:01 +0100

    PEQUEÑO CAMBIO ESTÉTICO ARREGLADO

--- resources/views/artista/create.blade.php
+++ resources/views/artista/create.blade.php
@@ -28,7 +28,7 @@
                                     class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                     id="nombre"
                                     type="text"
-                                    placeholder="Nombre Festival"
+                                    placeholder="Nombre Artista"
                                     name="nombre"
                                 />
                             </div>
