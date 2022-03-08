2bc6cd92022d7d907ba49c83f1e08a92c0104829
Alemol <91252055+AlemolAMG02@users.noreply.github.com>
2022-03-08 09:46:32 +0100

    Añadido campo Precio en el formulario. Podemos cambiar el precio de la entrada uno de S

--- resources/views/festival/edit.blade.php
+++ resources/views/festival/edit.blade.php
@@ -104,7 +104,7 @@
                                 value="{{$fest->fecha}}"
                             />
                         </div>
-                        <div class="mb-4 ">
+                        <div class="mb-4 md:flex md:justify-between ">
                             <div class="mb-4 md:mr-2 md:mb-0">
                                 <label class="block mb-2 text-sm font-bold text-gray-700" for="foto">
                                     Imagen
@@ -116,6 +116,19 @@
                                     placeholder="Imagen del coche"
                                     name="foto"
                                     value="{{$fest->imagen}}"
+                                />
+                            </div>
+                            <div class="mb-4 md:mr-2 md:mb-0">
+                                <label class="block mb-2 text-sm font-bold text-gray-700" for="precio">
+                                    Precio entrada
+                                </label>
+                                <input
+                                    class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                    id="precio"
+                                    type="number"
+                                    placeholder="Precio entrada"
+                                    name="precio"
+                                    value="{{$fest->precio}}"
                                 />
                             </div>
                         </div>
